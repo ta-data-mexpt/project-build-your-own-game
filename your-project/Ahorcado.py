ed79edc7cdaa4350b6dddc97b388fe2defc36908
Aimee Meza <aimeemezacursos@gmail.com>
2020-04-12 23:23:28 -0500

    palabras e imagenes

---
+++ your-project/Ahorcado.py
@@ -0,0 +1,90 @@
+import random
+
+Imagenes_ahorcado_10=[ '''
+
+
+========= 1''','''
+
+|   
+|   
+|   
+|
+|
+========= 2''','''
++---+
+|   
+|   
+|
+|
+|
+========= 3''','''
++---+
+|   |
+|   
+|  
+|  
+|   
+========= 4''','''
++---+
+|   |
+|   O
+|  
+|   
+|   
+========= 5''','''
++---+
+|   |
+|   O
+|   |
+|
+|
+=========6''','''
++---+
+|   |
+|   O
+|  /|
+|  
+|   
+=========7''','''
++---+
+|   |
+|   O
+|  /|\
+|  
+|   
+========= 8''','''
++---+
+|   |
+|   O
+|  /|\
+|  / 
+|   
+========= 9''','''
++---+
+|   |
+|   O
+|  /|\
+|  / \
+|   
+========= 10''']
+    
+def principal():
+    jugadores=input('Selecciona el numero de jugadores escribe 1 ó 2:')
+
+def numero_jugadores(n):
+
+def numero_rondas(n):
+
+def palabras(tema):
+    if tema == 1:
+        Astronomia4=['luna','NASA','star','roja']
+        Astronomia5=['Marte','Venus','Space','Urano']
+        Astronomia10=['planetoide','telescopio','astronomia','planetaria']
+    if tema == 2:
+        Biologia4=['agua','raiz','alga','aves']
+        Biologia5=['cinco','genes','pulpo','golgi']
+        Biologia10=['sanguineos','nutrientes','citoplasma','aminoacido']
+
+def palabra_azar(palabras(tema)):
+    
+    
